3ed5d19d3165883d4436833a0d9c79a712007419
Jamison Sasser <jtsasser@my.waketech.edu>
2022-02-23 21:32:54 -0500

    Create basic view

---
+++ src/Navigation/Routes.tsx
@@ -0,0 +1,7 @@
+import PantryScreen from '../Views/Pantry';
+import AddScreen from '../Views/Add';
+
+export default [
+  {name: 'Pantry', screen: PantryScreen, icon: null},
+  {name: 'Add', screen: AddScreen, icon: null},
+];
